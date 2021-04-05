3549cf29fc78b94df97ddfd3bd144ab2129b8a3c
胡文锋 <52076677+huwenfeng233@users.noreply.github.com>
2021-04-05 10:08:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,3 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
- git clone https://github.com/mchome/openwrt-dogcom.git
- git clone https://github.com/mchome/luci-app-dogcom.git
